18b1ed2efb6048803d7665c3c501a4206d112ed9
Alexey Malyavkin <a@malyavk.in>
2016-09-14 18:04:57 +0300

    expansion

--- Map tests.py
+++ Map tests.py
@@ -1,5 +1,19 @@
 import unittest
-from main import Metro,dict_to_list
+from main import Metro, dict_to_list
+
+STATION_NAMES = [
+    "Новоясеневская",
+    "Ясенево",
+    "Тёплый Стан",
+    "Коньково",
+    "Беляево",
+    "Калужская",
+    "Новые Черёмушки",
+    "Профсоюзная",
+    "Академическая",
+    "Ленинский проспект",
+]
+LINE_ID = 6
 
 
 class MapAnalyticsTestCase(unittest.TestCase):
@@ -31,17 +45,28 @@ class MapAnalyticsTestCase(unittest.TestCase):
         self.assertIn(int(st2['key']), line_neighbours_ids)
         self.assertIn(int(st3['key']), transfer_neighbours_ids)
 
+    def test_get_line_neighbour_ids(self):
+        m = Metro('data.json')
+        station = dict_to_list(m.get_stations_by_name("Киевская", line=4))[0]
+        neighbour_a = dict_to_list(m.get_stations_by_name("Смоленская", line=4))[0]
+        neighbour_b = dict_to_list(m.get_stations_by_name("Студенческая", line=4))[0]
+        neighbour_c = dict_to_list(m.get_stations_by_name("Выставочная", line=4))[0]
+        station_id = station["key"]
+        neighbours = m.get_line_neighbour_ids(station_id)
+        self.assertIn(neighbour_a["key"], neighbours)
+        self.assertIn(neighbour_b["key"], neighbours)
+        self.assertIn(neighbour_c["key"], neighbours)
+
     def test_line_neighbours_count(self):
         m = Metro('data.json')
         station_ids = m.get_all_station_ids()
 
         station = dict_to_list(m.get_stations_by_name("Киевская", line=4))[0]
-        except_station_no = station["key"]
+        except_station_id = station["key"]
 
         for station_id in station_ids:
-            neighbours = m.get_neighbours(station_id)
-            line_neighbours = [link["otherStationId"] for link in neighbours if link["type"] == "link"]
-            if station_id != except_station_no:
+            line_neighbours = m.get_line_neighbour_ids(station_id)
+            if station_id != except_station_id:
                 self.assertTrue(len(line_neighbours) in [1, 2])
             # testing get_neighbours for junction stations
             else:
@@ -52,8 +77,85 @@ class MapAnalyticsTestCase(unittest.TestCase):
         m = Metro('data.json')
         result = dict_to_list(m.get_stations_by_name(name))
         self.assertEqual(3, len(result))
+
+    def test_get_stations_by_name_line(self):
+        name = "Киевская"
+        m = Metro('data.json')
         result = dict_to_list(m.get_stations_by_name(name, line=4))
         self.assertEqual(1, len(result))
+
+    def test_continue_line(self):
+        m = Metro('data.json')
+        st1 = dict_to_list(m.get_stations_by_name("Новоясеневская"))[0]
+        st2 = dict_to_list(m.get_stations_by_name("Ясенево"))[0]
+        st3 = dict_to_list(m.get_stations_by_name("Тёплый Стан"))[0]
+        st3_id = m.continue_line(st1["key"], st2["key"])
+        self.assertEqual(st3_id, st3["key"])
+
+    def test_continue_line_end(self):
+        m = Metro('data.json')
+        st1 = dict_to_list(m.get_stations_by_name("Новоясеневская"))[0]
+        st2 = dict_to_list(m.get_stations_by_name("Ясенево"))[0]
+        st_none_id = m.continue_line(st2["key"], st1["key"])
+        self.assertIsNone(st_none_id)
+
+    def test_continue_line_junction(self):
+        m = Metro('data.json')
+        st1 = dict_to_list(m.get_stations_by_name("Киевская", line=4))[0]
+        st2 = dict_to_list(m.get_stations_by_name("Смоленская", line=4))[0]
+        st_none_id = m.continue_line(st2["key"], st1["key"])
+        self.assertIsNone(st_none_id)
+
+    def test_each_link_has_formed_key(self):
+        m = Metro('data.json')
+        links = m.links
+        for link in links:
+            _from = link[:-3]
+            _to = link[-3:]
+            self.assertEqual(int(_from), links[link]["fromStationId"])
+            self.assertEqual(int(_to), links[link]["toStationId"])
+
+
+class MapRoutingTestCase(unittest.TestCase):
+    def test_continue_line_until_transfer_or_end(self):
+        m = Metro('data.json')
+
+        station_ids = [dict_to_list(m.get_stations_by_name(station_name, line=LINE_ID))[0]["key"]
+                       for station_name in STATION_NAMES]
+
+        stack = m.continue_line_until_transfer_or_end(station_ids[0], station_ids[1])
+        self.assertListEqual(stack, station_ids)
+        stack = m.continue_line_until_transfer_or_end(station_ids[-1], station_ids[-2])
+        self.assertListEqual(stack, station_ids[::-1])
+
+    def test_get_line_segment_from_station(self):
+        m = Metro('data.json')
+        station_ids = [dict_to_list(m.get_stations_by_name(station_name, line=LINE_ID))[0]["key"]
+                       for station_name in STATION_NAMES]
+        non_transfer_station_ids = [station_id for station_id in station_ids
+                                    if not m.is_station_a_transfer_station(station_id)]
+        for station_id in non_transfer_station_ids:
+            stack = m.get_line_segment_from_station(station_id)
+            self.assertSetEqual(set(stack), set(station_ids))
+
+    def test_get_links(self):
+        m = Metro('data.json')
+        st1 = dict_to_list(m.get_stations_by_name("Новоясеневская"))[0]
+        st2 = dict_to_list(m.get_stations_by_name("Ясенево"))[0]
+        st3 = dict_to_list(m.get_stations_by_name("Битцевский парк"))[0]
+
+        links = m.get_links(st1["key"], st2["key"], include_transfers=False)
+        self.assertEqual(len(links), 1)
+        self.assertTrue(m.are_stations_adjacent(st1["key"], st2["key"], include_transfers=False))
+
+        links = m.get_links(st1["key"], st3["key"], include_transfers=False)
+        self.assertEqual(len(links), 0)
+        self.assertFalse(m.are_stations_adjacent(st1["key"], st3["key"], include_transfers=False))
+
+        links = m.get_links(st1["key"], st3["key"], include_transfers=True)
+        self.assertEqual(len(links), 1)
+        self.assertTrue(m.are_stations_adjacent(st1["key"], st3["key"], include_transfers=True))
+
 
 if __name__ == '__main__':
     unittest.main()
--- main.py
+++ main.py
@@ -20,48 +20,153 @@ class Metro:
                 self.run_checks(full_map)
                 self.full_map = full_map
 
+        self.links = full_map["links"]
+        self.stations = full_map['stations']
+
     def get_transfer_stations(self):
-        return [station_no for (station_no, station) in self.full_map['stations'].items()
+        return [station_id for (station_id, station) in self.stations.items()
                 if "isTransferStation" in station and station["isTransferStation"]]
 
     def get_not_transfer_stations(self):
         transfer_stations = self.get_transfer_stations()
-        return [station_no for station_no in self.get_all_station_ids()
-                if station_no not in transfer_stations]
+        return [station_id for station_id in self.get_all_station_ids()
+                if station_id not in transfer_stations]
 
     def get_all_station_ids(self):
-        return [station_no for (station_no, station) in self.full_map['stations'].items()]
+        return [station_id for (station_id, station) in self.stations.items()]
 
-    def get_station_by_no(self, station_no):
-        return self.full_map['stations'][str(station_no)]
+    def get_station_by_id(self, station_id):
+        return self.stations[str(station_id)]
 
     def get_stations_by_name(self, name, line=None):
-        candidates = {station_no: station for (station_no, station) in self.full_map['stations'].items()
+        candidates = {station_id: station for (station_id, station) in self.stations.items()
                       if station["name"] == name}
         if line:
-            candidates = {station_no: station for (station_no, station) in candidates.items()
+            candidates = {station_id: station for (station_id, station) in candidates.items()
                           if station['lineId'] == line}
 
         return candidates
 
-    def get_neighbours(self, station_no):
-        station = self.get_station_by_no(station_no)
+    def get_neighbours(self, station_id):
+        """
+        Return link objects for line neighbours and transfer neighbours
+        :param station_id:
+        :return:
+        """
+        station = self.get_station_by_id(station_id)
         link_ids = station["linkIds"]
         links = []
         for link_id in link_ids:
             link = self.full_map["links"][str(link_id)]
-            other_station_id = [link[k] for k in ["fromStationId", "toStationId"] if link[k] != int(station_no)][0]
+            other_station_id = [link[k] for k in ["fromStationId", "toStationId"] if link[k] != int(station_id)][0]
             new_link = {**link, "otherStationId": other_station_id}
             links.append(new_link)
         return links
 
-    # def get_closest_transfer_stations(self, station_no):
-    #    station = self.get_station(station_no)
+    def get_line_neighbour_ids(self, station_id):
+        neighbours = self.get_neighbours(station_id)
+        return [str(link["otherStationId"]) for link in neighbours if link["type"] == "link"]
+
+    def is_station_a_transfer_station(self, station_id):
+        neighbours = self.get_neighbours(station_id)
+        transfer_neighbours_ids = [link["otherStationId"] for link in neighbours if link["type"] == "transfer"]
+        return len(transfer_neighbours_ids) > 0
+
+    def is_station_a_termination_station(self, station_id):
+        neighbours = self.get_line_neighbour_ids(station_id)
+        return len(neighbours) == 1
+
+    def is_station_a_junction_station(self, station_id):
+        neighbours = self.get_line_neighbour_ids(station_id)
+        return len(neighbours) > 2
+
+    def continue_line(self, station_id_a, station_id_b):
+        """
+        Return id of station C in line:
+            ? -- A -- B -- C -- ?
+        Return None if B is the last station or if B is the junction station
+
+        :param station_id_a:
+        :param station_id_b:
+        :return:
+        """
+        if self.is_station_a_junction_station(station_id_b):
+            return None
+
+        if self.is_station_a_transfer_station(station_id_b):
+            return None
+
+        b_neighbours = self.get_line_neighbour_ids(station_id_b)
+        assert len(b_neighbours) in [1, 2]
+        other_station = [st_id for st_id in b_neighbours if st_id != station_id_a]
+        assert len(other_station) in [0, 1]
+        if len(other_station) == 0:
+            return None
+        if len(other_station) == 1:
+            return other_station[0]
+
+    def continue_line_until_transfer_or_end(self, station_id_a, station_id_b):
+        stack = [station_id_a, station_id_b]
+        while True:
+            next_station = self.continue_line(stack[-2], stack[-1])
+            if next_station and next_station != station_id_a:
+                stack.append(next_station)
+            else:
+                break
+        return stack
+
+    def get_line_segment_from_station(self, station_id):
+        """
+        Return metro line segment (list of ids) between two transfer stations containing said station,
+        including transfer stations
+        Return [station_id] if station_id is transfer station
+
+        Junction stations (currently only Киевская) are counted as transfer stations
+        :param station_id:
+        :return:
+        """
+        station = self.get_station_by_id(station_id)
+        if "isTransferStation" in station and station["isTransferStation"]:
+            return [station_id]
+        else:
+            if (self.is_station_a_junction_station(station_id) or
+                    self.is_station_a_transfer_station(station_id)):
+                return [station_id]
+            else:
+                neighbours = self.get_line_neighbour_ids(station_id)
+                assert len(neighbours) in [1, 2]
+                stacks = []
+                for neighbour in neighbours:
+                    stack = self.continue_line_until_transfer_or_end(station_id, neighbour)
+                    stacks.append(stack)
+                # line :      A - B ->C<- D - E
+                # stacks[0]   C - D - E
+                # stacks[1]   C - B - A
+                result_segment = stacks[0]
+                if len(stacks) == 2:
+                    second_stack = stacks[1][1:]
+                    result_segment = second_stack+result_segment
+                return result_segment
+
+    def get_links(self, station_id_a, station_id_b, include_transfers=False):
+        station_id_a = str(station_id_a)
+        station_id_b = str(station_id_b)
+        # every link has key like
+        #     XXXYYY or XXYYY
+        # where X and Y are station ids (Y has leading zeroes)
+        fmt = "{a}{b:0>3}"
+        possible_keys = [fmt.format(a=station_id_a, b=station_id_b),
+                         fmt.format(a=station_id_b, b=station_id_a)]
+
+        links = [self.links[key] for key in possible_keys if key in self.links]
+
+        if not include_transfers:
+            links = [link for link in links if link["type"] == "link"]
+
+        return links
+
+    def are_stations_adjacent(self, station_id_a, station_id_b, include_transfers=False):
+        return self.get_links(station_id_a, station_id_b, include_transfers) != []
 
 
-def run():
-    pass
 
-
-if __name__ == "__main__":
-    run()
